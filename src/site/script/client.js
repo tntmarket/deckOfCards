316815a336ea651782bfd8ab36a956770110a0fd
Dave Lu <dave792321001730003212345@gmail.com>
2012-01-26 05:00:56 -0500

    socket communication not working on heroku, try storing in variable on client to make sure

--- src/site/script/client.js
+++ src/site/script/client.js
@@ -1,13 +1,16 @@
 $(function() {
     var socket = io.connect('http://localhost');
-
+    var card, deck;
+    
     socket.emit('drawCard');
-
+    
     socket.on('getCard', function(data) {
+        card = data;
         console.log(data);
     });
 
     socket.on('spawnDeck', function(data) {
+        deck = data;
         console.log(data);
     });
 });
